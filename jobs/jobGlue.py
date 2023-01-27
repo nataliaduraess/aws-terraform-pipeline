c97a8768637082dbcb25a1f038027aa017a7f18a
Natalia <natalia.duraess@gmail.com>
2023-01-27 08:13:27 -0300

    project 1st load

--- jobs/jobGlue.py
+++ jobs/jobGlue.py
@@ -2,7 +2,9 @@ import sys
 from pyspark.context import SparkContext
 from awsglue.utils import getResolvedOptions
 from awsglue.context import GlueContext
-from awsglue.job import job
+from awsglue.job import Job
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, round
 
 #params: ['JOB_NAME']
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -13,7 +15,6 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-
 #Ler os dados do IMDB
 imdb = (
     spark
@@ -21,9 +22,10 @@ imdb = (
     .format("csv")
     .option("header", True)
     .option("inferSchema", True)
-    .option("delimiter", ";")
-    .load("s3://data-platform-bronze-prod/raw-database/")  
+    .option("delimiter", "\t")
+    .load("s3://data-platform-bronze-prod/raw-database/") 
 )    
+
 
 (
     imdb
@@ -31,4 +33,24 @@ imdb = (
     .mode("overwrite")
     .format("parquet")
     .save("s3://data-platform-silver-prod/parquet-data/")
+)
+
+imdb_parquet = (
+    spark
+    .read
+    .format("parquet")
+    .load("s3://data-platform-silver-prod/parquet-data/")
+)    
+
+imdb_parquet_col = (
+    imdb_parquet.select('tconst', 'primaryTitle', 'runtimeMinutes')
+    .withColumn("runtimeHours", round(col('runtimeMinutes').cast('int') / 60, 3))
+)
+
+(
+    imdb_parquet_col
+    .write
+    .mode("overwrite")
+    .format("parquet")
+    .save("s3://data-platform-gold-prod/consume-data/")
 )
